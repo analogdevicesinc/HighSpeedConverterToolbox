1bd7c8117cacfa744f259710473ec794f8060d22
Travis F. Collins <travis.collins@analog.com>
2019-09-05 16:32:37 -0700

    Add MATLAB HWA API for DAQ2 and test core
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

---
+++ hdl/vendor/AnalogDevices/+AnalogDevices/+daq2/+common/plugin_rd.m
@@ -0,0 +1,71 @@
+function hRD = plugin_rd(board, design)
+% Reference design definition
+
+% Construct reference design object
+hRD = hdlcoder.ReferenceDesign('SynthesisTool', 'Xilinx Vivado');
+
+% This is the base reference design that other RDs can build upon
+hRD.ReferenceDesignName = sprintf('DAQ2 %s (%s)', upper(board), upper(design));
+
+% Determine the board name based on the design
+hRD.BoardName = sprintf('AnalogDevices DAQ2 %s', upper(board));
+
+% Tool information
+hRD.SupportedToolVersion = {'2018.2'};
+
+% Get the root directory
+rootDir = fileparts(strtok(mfilename('fullpath'), '+'));
+
+% Design files are shared
+hRD.SharedRD = true;
+hRD.SharedRDFolder = fullfile(rootDir, 'vivado');
+
+%% Add custom design files
+% add custom Vivado design
+hRD.addCustomVivadoDesign( ...
+	'CustomBlockDesignTcl', fullfile('projects', 'daq2', lower(board), 'system_project_rxtx.tcl'), ...
+	'CustomTopLevelHDL',    fullfile('projects', 'daq2', lower(board), 'system_top.v'));
+
+hRD.BlockDesignName = 'system';
+
+% custom constraint files
+hRD.CustomConstraints = {...
+    fullfile('projects', 'daq2', lower(board), 'system_constr.xdc'), ...
+    fullfile('projects', 'common', lower(board), sprintf('%s_system_constr.xdc', lower(board))), ...
+    };
+
+% custom source files
+hRD.CustomFiles = {...
+    fullfile('projects')...,
+	fullfile('library')...,
+    };
+
+hRD.addParameter( ...
+    'ParameterID',   'ref_design', ...
+    'DisplayName',   'Reference Type', ...
+    'DefaultValue',  design);
+
+hRD.addParameter( ...
+    'ParameterID',   'fpga_board', ...
+    'DisplayName',   'FPGA Boad', ...
+    'DefaultValue',  upper(board));
+
+
+%% Add interfaces
+% add clock interface
+switch(upper(design))
+    case 'RX'
+        hRD.addClockInterface( ...
+            'ClockConnection',   'util_daq2_xcvr/rx_out_clk_0', ...
+            'ResetConnection',   'sys_rstgen/peripheral_aresetn');
+    case 'TX'
+        hRD.addClockInterface( ...
+            'ClockConnection',   'util_daq2_xcvr/tx_out_clk_0', ...
+            'ResetConnection',   'sys_rstgen/peripheral_aresetn');
+%     case 'RX & TX'
+%         hRD.addClockInterface( ...
+%             'ClockConnection',   'axi_adrv9009_rx_clkgen/clk_0', ...
+%             'ResetConnection',   'sys_rstgen/peripheral_aresetn');
+    otherwise
+        error('Unknown reference design');
+end
---
+++ test/board_variants.m
@@ -0,0 +1,16 @@
+function r = board_variants
+% Board plugin registration file
+% 1. Any registration file with this name on MATLAB path will be picked up
+% 2. Registration file returns a cell array pointing to the location of
+%    the board plugins
+% 3. Board plugin must be a package folder accessible from MATLAB path,
+%    and contains a board definition file
+
+%   Copyright 2012-2013 The MathWorks, Inc.
+
+r = { ...
+     'AnalogDevices.daq2.zcu102.plugin_rd_rx', ...
+     'AnalogDevices.daq2.zcu102.plugin_rd_tx' ...
+    };
+end
+% LocalWords:  Zynq ZC

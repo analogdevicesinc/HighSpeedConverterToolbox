48ea6e1b2fe9898c0b50f90ef91661f76768fa31
Travis F. Collins <travis.collins@analog.com>
2021-04-08 11:57:53 -0700

    Update failing tests for NonHW tests
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

--- test/NonHWTest.m
+++ test/NonHWTest.m
@@ -1,21 +1,27 @@
 classdef NonHWTest < matlab.unittest.TestCase
     
     properties(TestParameter)
-        rootClasses = {'AD9081','AD9144','AD9467'...
-            'AD9680','DAQ2','QuadMxFE'}
-        children = {'Rx','Tx'};
+        rootClasses = {...
+            {'AD9081',{'Rx','Tx'}},...
+            {'AD9144',{'Tx'}},...
+            {'AD9467',{'Rx'}},...
+            {'AD9680',{'Rx'}},...
+            {'DAQ2',{'Rx','Tx'}},...
+            {'QuadMxFE',{'Rx','Tx'}}...
+            };
     end
-    
-   
+        
     methods (Test)
         
-        function call_constructors(testCase,rootClasses,children)
-            sdr = eval(['adi.',rootClasses,'.',children,'()']);
-            testCase.assertEqual(class(sdr),['adi.',rootClasses,'.',children]);
+        function call_constructors(testCase,rootClasses)
+            for trx = rootClasses{2}
+                sdr = eval(['adi.',rootClasses{1},'.',trx{:},'()']);
+                testCase.assertEqual(class(sdr),['adi.',rootClasses{1},'.',trx{:}]);
+            end
         end
         
     end
-
+    
     
 end
 

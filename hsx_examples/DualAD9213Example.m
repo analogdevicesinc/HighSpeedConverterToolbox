51c70b67c67d6d7a87c5bc9cc31f5804cf58b066
Travis F. Collins <travis.collins@analog.com>
2022-08-08 15:51:15 -0600

    Add support for AD9213 (#78)
    * AD9213 - First commit
    
    Signed-off-by: Srikanth Pagadarai <srikanth.pagadarai@analog.com>
    
    * Added a basic example for using AD9213 system object
    
    Signed-off-by: Srikanth Pagadarai <srikanth.pagadarai@analog.com>
    
    * Added a basic example for using AD9213 system object
    
    Signed-off-by: Srikanth Pagadarai <srikanth.pagadarai@analog.com>
    
    * Add AD9213 to sysobj no-hw tests
    
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>
    
    Co-authored-by: Srikanth Pagadarai <srikanth.pagadarai@analog.com>

---
+++ hsx_examples/DualAD9213Example.m
@@ -0,0 +1,14 @@
+clear all;
+
+% Dual AD9213 system object
+rx = adi.AD9213.Rx('uri','ip:10.48.65.15');
+rx.EnabledChannels = [1 2];
+
+% Get a buffer
+x = rx();
+
+% Plot data
+figure; 
+plot(x);
+
+release(rx);

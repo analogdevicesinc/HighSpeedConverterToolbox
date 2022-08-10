694a4b398a875615a5a1d676dd8b550ddcad54f8
Travis F. Collins <travis.collins@analog.com>
2022-08-10 15:59:53 -0600

    Split AD9213 and DualAD9213
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

---
+++ +adi/+DualAD9213/Rx.m
@@ -0,0 +1,41 @@
+classdef Rx < adi.AD9213.Rx
+    % adi.DualAD9213.Rx Receive data from the DualAD9213 high speed ADC
+    %   The adi.DualAD9213.Rx System object is a signal source that can receive
+    %   complex data from the DualAD9213.
+    %
+    %   rx = adi.DualAD9213.Rx;
+    %   rx = adi.DualAD9213.Rx('uri','ip:192.168.2.1');
+    %
+    %   <a href="http://www.analog.com/media/en/technical-documentation/data-sheets/AD9213.pdf">AD9213 Datasheet</a>
+    %
+    %   See also adi.DAQ2.Rx
+           
+    methods
+        %% Constructor
+        function obj = Rx(varargin)
+            % Returns the matlabshared.libiio.base object
+            coder.allowpcode('plain');
+            obj = obj@adi.AD9213.Rx(varargin{:});
+            obj.channel_names = {'voltage0','voltage1'};
+        end
+    end   
+    
+    %% External Dependency Methods
+    methods (Hidden, Static)
+        
+        function tf = isSupportedContext(bldCfg)
+            tf = matlabshared.libiio.ExternalDependency.isSupportedContext(bldCfg);
+        end
+        
+        function updateBuildInfo(buildInfo, bldCfg)
+            % Call the matlabshared.libiio.method first
+            matlabshared.libiio.ExternalDependency.updateBuildInfo(buildInfo, bldCfg);
+        end
+        
+        function bName = getDescriptiveName(~)
+            bName = 'Dual-AD9213';
+        end
+        
+    end
+end
+

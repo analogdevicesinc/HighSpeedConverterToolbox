90f4c551648986ba96713487e03e7bd4d2f58525
Julia Pineda <Julia.Pineda@analog.com>
2022-09-13 19:36:53 +0800

    Add AD9081, DAQ3 to hw test pipeline
    Signed-off-by: Julia Pineda <Julia.Pineda@analog.com>

--- test/runHWTests.m
+++ test/runHWTests.m
@@ -1,13 +1,44 @@
 function runHWTests(board)
 
-    import matlab.unittest.TestRunner;
-    import matlab.unittest.TestSuite;
-    import matlab.unittest.plugins.TestReportPlugin;
-    import matlab.unittest.plugins.XMLPlugin
+import matlab.unittest.TestRunner;
+import matlab.unittest.TestSuite;
+import matlab.unittest.plugins.TestReportPlugin;
+import matlab.unittest.plugins.XMLPlugin
+import matlab.unittest.plugins.DiagnosticsValidationPlugin
+import matlab.unittest.parameters.Parameter
+import matlab.unittest.plugins.ToUniqueFile;
+import matlab.unittest.plugins.TAPPlugin;
+import matlab.unittest.constraints.ContainsSubstring;
+import matlab.unittest.selectors.HasName;
+import matlab.unittest.selectors.HasProcedureName;
+switch board
+    case {"socfpga-arria10-socdk-daq2", ...
+            "zynq-zc706-adv7511-fmcdaq2", ...
+            "zynqmp-zcu102-rev10-fmcdaq2"}
+        at = 'DAQ2';
+    case {"zynq-zc706-adv7511-fmcdaq3-revC", ...
+            "zynqmp-zcu102-rev10-fmcdaq3"}
+        at = 'DAQ3';
+    case {"zynqmp-zcu102-rev10-ad9081-v204b-txmode9-rxmode4", ...
+            "zynqmp-zcu102-rev10-ad9081-v204c-txmode0-rxmode1", ...
+            "zynqmp-zcu102-rev10-ad9081-vm4-l8", ...
+            "zynqmp-zcu102-rev10-ad9081-vm8-l4"}
+        at = 'AD9081';
+    otherwise
+        error('%s unsupported for HW test harness', board);
+end
 
+    ats = {'DAQ2Tests','DAQ3Tests','AD9081HWTests'};
+
+    if nargin == 0
+        suite = testsuite(ats);
+    else
+        suite = testsuite(ats);
+        suite = selectIf(suite,HasProcedureName(ContainsSubstring(at,'IgnoringCase',true)));
+    end
     try
-        suite = testsuite({'DAQ2Tests'});
-        runner = TestRunner.withNoPlugins;
+        runner = matlab.unittest.TestRunner.withTextOutput('OutputDetail',1);
+        runner.addPlugin(DiagnosticsValidationPlugin)
         xmlFile = board+"_HWTestResults.xml";
         plugin = XMLPlugin.producingJUnitFormat(xmlFile);
         
@@ -17,17 +48,20 @@ function runHWTests(board)
         t = table(results);
         disp(t);
         disp(repmat('#',1,80));
+        fid = fopen('failures.txt','a+');
         for test = results
             if test.Failed
                 disp(test.Name);
+                fprintf(fid,string(test.Name)+'\n');
             end
         end
+        fclose(fid);
     catch e
         disp(getReport(e,'extended'));
         bdclose('all');
         exit(1);
     end
     save(['BSPTest_',datestr(now,'dd_mm_yyyy-HH_MM_SS'),'.mat'],'t');
-    bdclose('all');
-    exit(any([results.Failed]));
+bdclose('all');
+exit(any([results.Failed]));
 end

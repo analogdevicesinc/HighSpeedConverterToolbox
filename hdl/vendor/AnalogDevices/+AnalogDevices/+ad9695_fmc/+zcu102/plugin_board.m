cac32abae4abfbca1aa072ece5e16d5ade7afbec
Ionut.Muthi <imuthi@romlx8.adlk.analog.com>
2022-09-26 09:44:53 +0100

    work in progress

---
+++ hdl/vendor/AnalogDevices/+AnalogDevices/+ad9695_fmc/+zcu102/plugin_board.m
@@ -0,0 +1,7 @@
+function hP = plugin_board()
+% Zynq Platform PCore
+% Use Plugin API to create board plugin object
+
+% Call the common board definition function
+hP = AnalogDevices.plugin_board('AD9695', 'ZCU102');
+

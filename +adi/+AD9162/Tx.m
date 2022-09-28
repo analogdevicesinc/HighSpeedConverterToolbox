0294a5883224b1263c2cfdbe455aab5222eb88ce
Ionut.Muthi <imuthi@romlx8.adlk.analog.com>
2022-09-28 08:38:39 +0100

    add streaming support for FMCOMMS11

---
+++ +adi/+AD9162/Tx.m
@@ -0,0 +1,58 @@
+classdef Tx < adi.common.Tx & adi.AD9162.Base & adi.common.DDS
+    % adi.AD9162.Tx Transmit data to the AD9162 high speed DAC
+    %
+    %   tx = adi.AD9162.Tx;
+    %   tx = adi.AD9162.Tx('uri','192.168.2.1');
+    %
+    %   <a href="http://www.analog.com/media/en/technical-documentation/data-sheets/AD9161-9162.pdf">AD9162 Datasheet</a>
+    %
+    %   See also adi.FMCOMMS11.Tx
+    
+    properties (Constant)
+        %SamplingRate Sampling Rate
+        %   Baseband sampling rate in Hz, specified as a scalar 
+        %   in samples per second. This value is constant
+        SamplingRate = 1e9;
+    end
+    
+    properties (Hidden, Nontunable, Access = protected)
+        isOutput = true;
+    end
+    
+    properties(Nontunable, Hidden, Constant)
+        Type = 'Tx';
+    end
+    
+    properties (Nontunable, Hidden)
+        devName = 'axi-ad9162-hpc';
+        channel_names = {'voltage0_i','voltageo_q'};
+    end
+    
+    methods
+        %% Constructor
+        function obj = Tx(varargin)
+            % Returns the matlabshared.libiio.base object
+            coder.allowpcode('plain');
+            obj = obj@adi.AD9162.Base(varargin{:});
+        end
+    end
+    
+    %% External Dependency Methods
+    methods (Hidden, Static)
+        
+        function tf = isSupportedContext(bldCfg)
+            tf = matlabshared.libiio.ExternalDependency.isSupportedContext(bldCfg);
+        end
+        
+        function updateBuildInfo(buildInfo, bldCfg)
+            % Call the matlabshared.libiio.method first
+            matlabshared.libiio.ExternalDependency.updateBuildInfo(buildInfo, bldCfg);
+        end
+        
+        function bName = getDescriptiveName(~)
+            bName = 'AD9162';
+        end
+        
+    end
+end
+

d9a97948159ee54327280ac21fec69b9ff2d2316
Stanca Pop <stanca.pop@analog.com>
2022-12-13 16:08:00 +0000

    Remove sysdef

--- CI/scripts/adi_build.tcl
+++ CI/scripts/adi_build.tcl
@@ -27,7 +27,7 @@ set sdk_loc vivado_prj.sdk
 # Export the hdf
 file delete -force $sdk_loc
 file mkdir $sdk_loc
-file copy -force vivado_prj.runs/impl_1/system_top.bit $sdk_loc/system_top.xsa
+write_hw_platform -fixed -force  -include_bit -file $sdk_loc/system_top.xsa
 
 # Close the Vivado project
 close_project
@@ -35,45 +35,62 @@ close_project
 # Create the BOOT.bin
 #exec xsdk -batch -source $cdir/projects/scripts/fsbl_build.tcl -tclargs $fpga_board -wait
 
-file mkdir $cdir/boot
+#file mkdir $cdir/boot
+#if {$fpga_board eq "ZCU102"} {
+#    #exec hsi -source $cdir/projects/scripts/pmufw_zynqmp.tcl
+#    if {[catch { [exec xsct $cdir/projects/scripts/pmufw_zynqmp.tcl]} msg]} {
+#      puts "$msg"
+#    }
+#    puts "end of PMUFW build"
+#    file copy -force $cdir/projects/scripts/fixmake.sh $cdir/fixmake.sh
+#    exec chmod +x fixmake.sh
+#
+#    #exec ./fixmake.sh
+#    #cd pmufw
+#    #exec make
+#    #cd ..
+#    if [catch "exec -ignorestderr ./fixmake.sh" ret opt] {
+#        set makeRet [lindex [dict get $opt -errorcode] end]
+#        puts "make returned with $makeRet"
+#    }
+#    if {[file exist pmufw/executable.elf] eq 0} {
+#        puts "ERROR: pmufw not built"
+#        return -code error 10
+#    } else {
+#        puts "pmufw built correctly!"
+#    }
+#
+#    set vversion [version -short]
+#    exec hsi -source $cdir/projects/scripts/fsbl_build_zynqmp.tcl $vversion
+#    if {[file exist boot/BOOT.BIN] eq 0} {
+#        puts "ERROR: BOOT.BIN not built"
+#        return -code error 11
+#    } else {
+#        puts "BOOT.BIN built correctly!"
+#    }
+#
+#} else {
+#    exec hsi -source $cdir/projects/scripts/fsbl_build_zynq.tcl
+#    if {[file exist boot/BOOT.BIN] eq 0} {
+#        puts "ERROR: BOOT.BIN not built"
+#        return -code error 11
+#    } else {
+#        puts "BOOT.BIN built correctly!"
+#    }
+#}
+
+set xsct_script "exec xsct ../../../hdl/vendor/AnalogDevices/vivado/projects/scripts/adi_make_boot_bin.tcl"
 if {$fpga_board eq "ZCU102"} {
-    exec hsi -source $cdir/projects/scripts/pmufw_zynqmp.tcl
-    file copy -force $cdir/projects/scripts/fixmake.sh $cdir/fixmake.sh
-    exec chmod +x fixmake.sh
-
-    #exec ./fixmake.sh
-    #cd pmufw
-    #exec make
-    #cd ..
-    if [catch "exec -ignorestderr ./fixmake.sh" ret opt] {
-        set makeRet [lindex [dict get $opt -errorcode] end]
-        puts "make returned with $makeRet"
-    }
-    if {[file exist pmufw/executable.elf] eq 0} {
-        puts "ERROR: pmufw not built"
-        return -code error 10
-    } else {
-        puts "pmufw built correctly!"
-    }
-
-    set vversion [version -short]
-    exec xsdk -batch -source $cdir/projects/scripts/fsbl_build_zynqmp.tcl $vversion
-    if {[file exist boot/BOOT.BIN] eq 0} {
-        puts "ERROR: BOOT.BIN not built"
-        return -code error 11
-    } else {
-        puts "BOOT.BIN built correctly!"
-    }
-
+  set uboot_elf $cdir/projects/common/boot/u-boot-zcu.elf
+  set arm_tr_frm_elf $cdir/projects/common/boot/bl31.elf
 } else {
-    exec xsdk -batch -source $cdir/projects/scripts/fsbl_build_zynq.tcl
-    if {[file exist boot/BOOT.BIN] eq 0} {
-        puts "ERROR: BOOT.BIN not built"
-        return -code error 11
-    } else {
-        puts "BOOT.BIN built correctly!"
-    }
+  set uboot_elf $cdir/projects/common/boot/u-boot.elf
 }
+set build_args "$sdk_loc/system_top.xsa $uboot_elf $cdir/boot $arm_tr_frm_elf"
+puts "Please wait, this may take a few minutes."
+eval $xsct_script $build_args
+
+../../../hdl/vendor/AnalogDevices/vivado/projects/scripts/adi_make_boot_bin.tcl
 
 puts "------------------------------------"
 puts "Embedded system build completed."

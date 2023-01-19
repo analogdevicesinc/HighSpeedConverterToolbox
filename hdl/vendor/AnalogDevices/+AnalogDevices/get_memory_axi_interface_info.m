355e35f42fd20eef6655a82c2e450da8e7b6bb83
Travis F. Collins <travis.collins@analog.com>
2023-01-19 08:34:33 -0700

    Fix memory address for VCU118 design
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

--- hdl/vendor/AnalogDevices/+AnalogDevices/get_memory_axi_interface_info.m
+++ hdl/vendor/AnalogDevices/+AnalogDevices/get_memory_axi_interface_info.m
@@ -70,7 +70,7 @@ switch project
         switch fpga
             case {'VCU118'}
                 InterfaceConnection = 'axi_cpu_interconnect/M17_AXI';
-                BaseAddress = '0x80000000';
+                BaseAddress = '0xFF0000';
                 MasterAddressSpace = 'sys_mb/Data';
             otherwise
                 error(sprintf('Unknown Project FPGA %s/%s',project,fpga)); %#ok<*SPERR>

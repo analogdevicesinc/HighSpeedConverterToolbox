789bab067bf0922829a5127cd204161c4ddd88e3
Stanca Pop <stanca.pop@analog.com>
2023-01-26 13:24:06 +0200

    Temp commit: Use OOC synthesis

--- CI/scripts/adi_build.tcl
+++ CI/scripts/adi_build.tcl
@@ -11,18 +11,21 @@ if {[info exists fpga_board]} {
 
 set prj_carrier $project$carrier
 
+# Define local variables
+set cdir [pwd]
+set sdk_loc vivado_prj.sdk
+set project_system_dir vivado_prj.srcs/sources_1/bd/system
+
 # Build the project
 update_compile_order -fileset sources_1
 reset_run impl_1
 reset_run synth_1
+set_property synth_checkpoint_mode Hierarchical [get_files $project_system_dir/system.bd]
+export_ip_user_files -of_objects [get_files $project_system_dir/system.bd] -no_script -sync -force -quiet
 launch_runs synth_1
 wait_on_run synth_1
 launch_runs impl_1 -to_step write_bitstream
 wait_on_run impl_1
-
-# Define local variables
-set cdir [pwd]
-set sdk_loc vivado_prj.sdk
 
 # Export the hdf
 file delete -force $sdk_loc

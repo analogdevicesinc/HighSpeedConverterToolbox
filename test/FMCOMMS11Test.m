e197305d1cb59098203d5c64330c9da20c0692dd
Julia Pineda <Julia.Pineda@analog.com>
2023-08-16 10:27:32 +0800

    [FIX] Use estFrequencyMax
    Signed-off-by: Julia Pineda <Julia.Pineda@analog.com>

--- test/FMCOMMS11Test.m
+++ test/FMCOMMS11Test.m
@@ -1,9 +1,36 @@
-classdef FMCOMMS11Test < matlab.unittest.TestCase
+classdef FMCOMMS11Test < HardwareTests
     properties
         uri ='ip:analog.local';
         author = 'ADI';
     end
 
+    methods(TestClassSetup)
+        % Check hardware connected
+        function CheckForHardware(testCase)
+            Device = @()adi.FMCOMMS11.Rx;
+            testCase.CheckDevice('ip',Device,testCase.uri(4:end),false);
+        end
+    end
+    
+    methods (Static)
+        function estFrequency(data,fs)
+            nSamp = length(data);
+            FFTRxData  = fftshift(10*log10(abs(fft(data))));
+%             df = fs/nSamp;  freqRangeRx = (-fs/2:df:fs/2-df).'/1000;
+%             plot(freqRangeRx, FFTRxData);
+            df = fs/nSamp;  freqRangeRx = (0:df:fs/2-df).'/1000;
+            plot(freqRangeRx, FFTRxData(end-length(freqRangeRx)+1:end,:));
+        end
+        
+        function freq = estFrequencyMax(data,fs)
+            nSamp = length(data);
+            FFTRxData  = fftshift(10*log10(abs(fft(data))));
+            df = fs/nSamp;  freqRangeRx = (0:df:fs/2-df).';
+            [~,ind] = max(FFTRxData(end-length(freqRangeRx)+1:end,:));
+            freq = freqRangeRx(ind);
+        end
+        
+    end
 
     methods(Test)
         function testFMCOMMS11Rx(testCase)
@@ -31,12 +58,13 @@ classdef FMCOMMS11Test < matlab.unittest.TestCase
             for k=1:10
                 [out, valid] = rx();
             end
-            freqEst = meanfreq(double(real(out)),rx.SamplingRate);
+%             freqEst = meanfreq(double(real(out)),rx.SamplingRate);
+            freqEst = testCase.estFrequencyMax(out(:,1),rx.SamplingRate);
             rx.release();
 
             testCase.verifyTrue(valid);
             testCase.verifyGreaterThan(sum(abs(double(out))),0);
-            testCase.verifyEqual(freqEst,toneFreq,'RelTol',0.01,...
+            testCase.verifyEqual(double(freqEst),toneFreq,'RelTol',0.01,...
                 'Frequency of DDS tone unexpected')
         end
     end

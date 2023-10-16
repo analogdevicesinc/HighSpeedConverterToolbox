722d5f38c21a682418923b7954d7fc3bf43df0ea
Travis F. Collins <travis.collins@analog.com>
2023-10-16 12:27:12 -0600

    [AD9081] Make sure datapath is in powered up state
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

--- +adi/+AD9081/Rx.m
+++ +adi/+AD9081/Rx.m
@@ -225,6 +225,9 @@ classdef Rx < adi.AD9081.Base & adi.common.Rx & adi.common.Attribute
                 obj.writeFilterFile();
             end
             %%
+            % Make sure device is powered up first otherwise JESD will be
+            % off
+            obj.setDeviceAttributeRAW('powerdown','0');
             obj.setDeviceAttributeRAW('jesd204_fsm_ctrl',obj.JESD204FSMControl);
             %%
             obj.setAttributeRAW('voltage0_i','test_mode',obj.TestMode,...
